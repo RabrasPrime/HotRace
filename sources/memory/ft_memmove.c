ca950eb4e7562f5a351965290c72e4a4413c6a4b
tjooris <tom.jooris@hotmail.fr>
2026-03-01 19:32:28 +0100

    norming memmove

--- sources/memory/ft_memmove.c
+++ sources/memory/ft_memmove.c
@@ -6,35 +6,41 @@
 /*   By: abetemps <abetemps@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/01 15:00:23 by abetemps          #+#    #+#             */
-/*   Updated: 2026/03/01 18:52:38 by tjooris          ###   ########.fr       */
+/*   Updated: 2026/03/01 19:32:12 by tjooris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hotrace.h"
 
+static void	ft_memmove_small(uint8_t *d, const uint8_t *s, size_t n)
+{
+	size_t	i;
+
+	if (s >= d)
+	{
+		i = 0;
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else
+		while (n--)
+			d[n] = s[n];
+}
+
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	const uint8_t	*s = (const uint8_t *)src;
 	uint8_t			*d;
-	size_t			i;
 
 	d = (uint8_t *)dest;
 	if (n == 0 || dest == src)
 		return (dest);
 	if (n < 8)
 	{
-		if (s >= (uint8_t *)dest)
-		{
-			i = 0;
-			while (i < n)
-			{
-				d[i] = s[i];
-				i++;
-			}
-		}
-		else
-			while (n--)
-				d[n] = s[n];
+		ft_memmove_small(d, s, n);
 		return (dest);
 	}
 	__asm__ volatile (
